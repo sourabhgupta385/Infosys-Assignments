89344bae34309e2a23246d49ec22e811fc22cc74
sourabhgupta385 <gupta.sourabh385@gmail.com>
2018-04-19 00:22:45 +0530

    Assignment 5 added

---
+++ Module_1/Section_3/Assignment5.py
@@ -0,0 +1,14 @@
+'''
+Created on 24-Mar-2018
+
+@author: SOURABH GUPTA
+'''
+'''
+Execute the following commands and observe the output.
+1) print("Hello World")
+2) print("Rahul's age is", 45)
+3) print("I have", 10, "mangoes and", 12, "bananas")
+'''
+print("Hello World")
+print("Rahul's age is", 45)
+print("I have", 10, "mangoes and", 12, "bananas")
