877b709578ae4159fcfd398dc27459727c8d1bac
sourabhgupta385 <gupta.sourabh385@gmail.com>
2018-04-21 23:08:27 +0530

    Assignment 15 added

---
+++ Module_1/Section_5/Assignment15.py
@@ -0,0 +1,20 @@
+'''
+@author: SOURABH GUPTA
+'''
+'''
+Create four string variables a, b, c, d to store the following values and display them:
+• My city is Mexico
+• Raghu is my friend's brother
+• My favorite programming language is "Python"
+• Python is a widely used high-level, general-purpose, interpreted, dynamic programming language.
+  It's design philosophy emphasizes code readability, and it's syntax allows programmers to express
+  concepts in fewer lines of code than possible in languages such as "C++" or "Java". 
+'''
+a = "My city is Mexico"
+b = "Raghu is my friend's brother"
+c = '''My favorite programming language is "Python"'''
+d = '''Python is a widely used high-level, general-purpose, interpreted, dynamic programming language.It's design philosophy emphasizes code readability, and it's syntax allows programmers to express concepts in fewer lines of code than possible in languages such as "C++" or "Java".'''
+print(a)
+print(b)
+print(c)
+print(d)
