b949e7f22e62f275c5b2a9fe8df3f564e718324a
sourabhgupta385 <gupta.sourabh385@gmail.com>
2018-04-26 00:20:30 +0530

    Assignment 22,23 added

--- Module_1/Section_5/Assignment22.py
+++ Module_1/Section_5/Assignment22.py
@@ -1,7 +1,15 @@
 '''
-Created on 25-Mar-2018
-
 @author: SOURABH GUPTA
+'''
+'''
+Consider the list of courses opted by a Student "John" and available electives at ABC Training Institute:
+courses = ("Python Programming", "RDBMS", "Web Technology", "Software Engg.")
+electives = ("Business Intelligence", "Big Data Analytics")
+Write a Python Program to satisfy business requirements mentioned below:
+1.List the number of courses opted by John.
+2.List all the courses opted by John.
+3.John is also interested in elective courses mentioned above.
+Print the updated tuple including electives.
 '''
 courses = ("Python Programming", "RDBMS", "Web Technology", "Software Engg.")
 electives = ("Business Intelligence", "Big Data Analytics")
--- Module_1/Section_5/Assignment23.py
+++ Module_1/Section_5/Assignment23.py
@@ -1,7 +1,17 @@
 '''
-Created on 25-Mar-2018
-
 @author: SOURABH GUPTA
+'''
+'''
+Given below is a dictionary 'customer_details' representing customer details from a Retail Application.
+Customer Id is the key and Customer Name is the value. 
+customer_details = { 1001 : "John", 1004 : "Jill", 1005: "Joe", 1003 : "Jack" }
+Write Python code to perform the operations mentioned below:
+a)Print details of customers.
+b)Print number of customers.
+c)Print customer names in ascending order.
+d)Delete the details of customer with customer id = 1005 and print updated dictionary.
+e)Update the name of customer with customer id = 1003 to "Mary" and print updated dictionary.
+f)Check whether details of customer with customer id = 1002 exists in the dictionary.
 '''
 customer_details = { 1001 : "John", 1004 : "Jill", 1005: "Joe", 1003 : "Jack" }
 print(customer_details)
