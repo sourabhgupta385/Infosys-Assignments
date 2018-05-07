479c15deebda9ddf5916d7b493043c15c2933f64
sourabhgupta385 <gupta.sourabh385@gmail.com>
2018-05-07 22:12:08 +0530

    Assignment 37, 38 of section 8 added

--- Module_1/Section_8/Assignment37.py
+++ Module_1/Section_8/Assignment37.py
@@ -1,16 +1,20 @@
 '''
-Created on 26-Mar-2018
-
 @author: SOURABH GUPTA
+'''
+'''
+You have already executed the Python program given below in Functions section:
+•Add natural numbers up to n where n is taken as an input from user.
+
+Do appropriate exception handling in the code and observe the output by providing invalid input values.
 '''
 def add_number(n):
     sum = 0
     for i in range(1,n+1):
         sum = sum + i
-    return sum   
+    return sum
 try:
     print(add_number(5))
 except TypeError:
     print("INVALID INPUT")
 else:
-    print("Success")        
+    print("Success")
--- Module_1/Section_8/Assignment38.py
+++ Module_1/Section_8/Assignment38.py
@@ -1,7 +1,46 @@
 '''
-Created on 26-Mar-2018
-
 @author: SOURABH GUPTA
+'''
+'''
+Refer to the following assignment which you have already executed in Functions section.
+Modify your code to implement Exception Handling and display appropriate error message wherever applicable.At an airport,
+a traveler is allowed entry into the ﬂight only if he clears the following checks:
+1.Baggage Check
+2.Immigration Check
+3.Security Check
+
+The logic for the check methods are given below:
+
+check_baggage (baggage_weight)•
+returns True if baggage_weightis greater than or equal to 0 and less than or equal to 40.
+Otherwise returns False.
+
+check_immigration (expiry_year)
+•returns True if expiry_yearis greater than or equal to 2001 and less than or equal to 2025.
+Otherwise returns False.
+
+check_security(noc_status)
+•returns True if noc_statusis 'valid' or 'VALID', for all other values return False.
+
+traveler()
+
+•Initialize the traveler Id and traveler name and invoke the functions check_baggage(),
+check_immigration() and check_security() by passing required arguments.
+•Refer the table below for values of arguments.
+    variable            values
+    traveler_Id         1001
+    traveler_name       Jim
+    baggage_weight      Thirty
+    expiry_year         2016
+    noc_status          0
+•If all values of check_baggage(), check_immigration() and check_security() are true,
+display traveler_id and traveler_name
+display "Allow Traveler to ﬂy!"
+Otherwise,
+display traveler_id and traveler_name
+display "Detain Traveler for Re-checking!“
+Invoke the traveler() function.
+Modify the values of diﬀerent variables in traveler() function and observe the output
 '''
 def check_baggage(baggage_weight):
     if baggage_weight >= 0 and baggage_weight <= 40:
@@ -12,7 +51,7 @@ def check_baggage(baggage_weight):
 def check_immigration(expiry_year):
     if expiry_year >= 2001 and expiry_year <= 2025:
         return True
-    
+
     else:
         return False
 
@@ -28,11 +67,11 @@ def traveler():
     print(traveler_Id)
     print(traveler_name)
     try:
-        if check_baggage(35) and check_immigration(2019) and check_security("VALID"):   
+        if check_baggage(35) and check_immigration(2019) and check_security("VALID"):
             print("Allow Traveler to ﬂy")
-        else: 
+        else:
             print("Detain Traveler for Re-checking")
     except TypeError:
         print("Invalid input")
-      
-traveler()        
+
+traveler()
